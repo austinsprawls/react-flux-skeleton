e655106a589b29149f180c5f9137f5a55c3d4d3a
Austin Sprawls <austinsprawls@Opss-MBP-4.attlocal.net>
2015-12-16 14:15:39 -0600

    added boilerplate code for Constants file

--- src/js/Constants.js
+++ src/js/Constants.js
@@ -1,3 +1,14 @@
 /**
  * Created by austinsprawls on 12/16/15.
  */
+import keyMirror from 'react/lib/keyMirror';
+
+export default {
+    // event name triggered from store, listened to by views
+    CHANGE_EVENT: 'change',
+
+    // Each time you add an action, add it here... They should be past-tense
+    ActionTypes: keyMirror({
+        TASK_ADDED: null
+    })
+};
